665ddb88581a1d687d06d53dedf1e24e22cb008f
Zaina <Zaina@DESKTOP-EI3LB00>
2023-04-06 01:38:51 -0400

    Fixed Nav bar

--- tuneMe/apps/home/routes.py
+++ tuneMe/apps/home/routes.py
@@ -16,7 +16,7 @@ def index():
     return render_template('home/index.html', segment='index')
 
 
-@blueprint.route('/<template>')
+@blueprint.route('/<template>', methods=['GET', 'POST'])
 @login_required
 def route_template(template):
 
